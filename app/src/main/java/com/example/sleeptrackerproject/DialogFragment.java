5be9681c0adf3168006e4e7a14209b66e6c49b70
Jakub Ogar <okazysssie@gmail.com>
2023-01-28 15:46:14 +0100

    cleaned up the dialog class

--- app/src/main/java/com/example/sleeptrackerproject/DialogFragment.java
+++ app/src/main/java/com/example/sleeptrackerproject/DialogFragment.java
@@ -2,7 +2,6 @@ package com.example.sleeptrackerproject;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,15 +14,11 @@ public class DialogFragment extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Information")
+        builder.setTitle("Alarm")
                 .setMessage("It's time to wake up!")
-                .setPositiveButton("Turn off", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // leaving it empty so system dismises it
-                    }
+                .setPositiveButton("Turn off", (dialogInterface, i) -> {
+                    // leaving it empty so system dismises it
                 });
-
         return builder.create();
     }
 }
