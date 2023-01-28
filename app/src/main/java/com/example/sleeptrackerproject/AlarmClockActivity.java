8176d8612c881910b843cbd8e647060d5a87b00e
Jakub Ogar <okazysssie@gmail.com>
2023-01-28 13:46:52 +0100

    added basic alarm clock functionality

---
+++ app/src/main/java/com/example/sleeptrackerproject/AlarmClockActivity.java
@@ -0,0 +1,69 @@
+package com.example.sleeptrackerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+
+import java.util.Calendar;
+
+public class AlarmClockActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private Button setAlarmButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_clock);
+
+        timePicker = findViewById(R.id.time_picker);
+        setAlarmButton = findViewById(R.id.set_alarm_button);
+
+        // set the initial value for the time picker
+        Calendar calendar = Calendar.getInstance();
+        timePicker.setHour(calendar.get(Calendar.HOUR_OF_DAY));
+        timePicker.setMinute(calendar.get(Calendar.MINUTE));
+
+        setAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hour = timePicker.getHour();
+                int minute = timePicker.getMinute();
+                setAlarm(hour, minute);
+            }
+        });
+    }
+
+    private void setAlarm(int hour, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        // if the desired hour has already passed today, set the alarm for the next day
+        if (calendar.before(Calendar.getInstance())) {
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        Intent intent = new Intent(AlarmClockActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(AlarmClockActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager != null) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        }
+
+        Toast.makeText(AlarmClockActivity.this, "Alarm set for " + hour + ":" + minute, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/sleeptrackerproject/AlarmReceiver.java
@@ -0,0 +1,31 @@
+package com.example.sleeptrackerproject;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, AlarmClockActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "my_channel_id")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Alarm")
+                .setContentText("Time to wake up!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(123, builder.build());
+    }
+}
+
